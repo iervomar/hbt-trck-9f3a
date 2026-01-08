2385b593702382a9848a86c22933909ec5aa7c0d
iervomar <86080507+iervomar@users.noreply.github.com>
2026-01-08 21:33:39 +0100

    Add initial habits array to app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const habits = [
+  { id: "med", name: "Meditation", target: 3 },
+  { id: "read", name: "Reading", target: 5 },
+  { id: "de", name: "German", target: 2 },
+  { id: "uke", name: "Ukulele / Creative", target: 2 },
+  { id: "proj", name: "Personal Project", target: 1 },
+  { id: "tv", name: "TV (max)", target: 3 }
+];
